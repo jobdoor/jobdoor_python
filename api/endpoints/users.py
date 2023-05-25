47c547330516502908c69ca248a9b2a763f21dc4
alejomaar <demonmaar@gmail.com>
2023-05-24 23:02:29 -0500

    feat: integrate cloud service

--- api/endpoints/users.py
+++ api/endpoints/users.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import Session
 from dependencies.deps import get_db
 import schemas
 import crud
-from dependencies.deps import get_db
 
 router = APIRouter()
 
@@ -33,4 +32,25 @@ def create_user(
     """
     
     user = crud.user.create(db, obj_in=user_in)
+    return user
+
+
+@router.put("/{user_id}", response_model=schemas.User)
+def update_user(
+    *,
+    db: Session = Depends(get_db),
+    user_id: int,
+    user_in: schemas.UserUpdate,
+) -> Any:
+    """
+    Update a user.
+    """
+    user = crud.user.get(db, id=user_id)
+    print(user)
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="The user with this username does not exist in the system",
+        )
+    user = crud.user.update(db, db_obj=user, obj_in=user_in)
     return user
--- core/config.py
+++ core/config.py
@@ -1,27 +1,26 @@
 import secrets
 from typing import Any, Dict, List, Optional, Union
-
+import os
+from dotenv import load_dotenv,find_dotenv
 from pydantic import AnyHttpUrl,  HttpUrl
 
+load_dotenv(find_dotenv())
 
+print()
+#os.getenv('REPORT_CONN_DRIVER','')
 class Settings():
     API_V1_STR: str = "/api/v1"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     SERVER_NAME: str
     SERVER_HOST: AnyHttpUrl
-    # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
-    # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
-    # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
     PROJECT_NAME: str ='jobdoors backend'
 
-
-    POSTGRES_SERVER: str = 'postgresql://postgres:password@localhost'
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
-    SQLALCHEMY_DATABASE_URI: str= 'postgresql://postgres:admin@localhost/jobdoor'
+    SQLALCHEMY_DATABASE_URI: str= os.getenv('DATABASE_URI_PROD','')
 
 
 
